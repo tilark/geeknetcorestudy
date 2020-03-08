999442518e3da5b8ec8b6dd3207b971c11333940
tilark <tilark.liu@gmail.com>
2020-03-08 21:15:21 +0800

    使用强类型对象承载配置数据
    1.  添加依赖包``Microsoft.Extensions.Configuration.Binder``
    2. 设置强类型
    ```
    class Config
    {
        public string Key1 { get; set; }
        public bool Key5 { get; set; }
        public int Key6 { get; set; }
    }
    ```
    3. 使用Bind。``configurationRoot.Bind(config);``
    4.  给私有化属性赋值，添加参数。```
    configurationRoot.GetSection("OrderService").Bind(config,
        binderOptions => { binderOptions.BindNonPublicProperties = true; }
        );
    ```

--- ConfiguraionFileDemo2/Program.cs
+++ ConfiguraionFileDemo2/Program.cs
@@ -14,18 +14,39 @@ namespace ConfiguraionFileDemo2
 
             var configurationRoot = builder.Build();
 
-            IChangeToken token = configurationRoot.GetReloadToken();
+            var config = new Config()
+            {
+                Key1 = "config key1",
+                Key5 = false
+            };
 
-            ChangeToken.OnChange(() =>
-                configurationRoot.GetReloadToken(),
-                () =>
-                {
-                    Console.WriteLine($"Key1: {configurationRoot["key1"]}");
-                    Console.WriteLine($"Key2: {configurationRoot["key2"]}");
-                    Console.WriteLine($"key3: {configurationRoot["key3"]}");
-                }
-            );
-            Console.WriteLine("Begin");
+            //configurationRoot.Bind(config);
+            configurationRoot.GetSection("OrderService").Bind(config,
+                binderOptions => { binderOptions.BindNonPublicProperties = true; }
+                );
+            Console.WriteLine($"Key1: {config.Key1}");
+            Console.WriteLine($"Key2: {config.Key5}");
+            Console.WriteLine($"key3: {config.Key6}");
+
+
+            #region ChangeToken.OnChange
+
+
+            //IChangeToken token = configurationRoot.GetReloadToken();
+
+            //ChangeToken.OnChange(() =>
+            //    configurationRoot.GetReloadToken(),
+            //    () =>
+            //    {
+            //        Console.WriteLine($"Key1: {configurationRoot["key1"]}");
+            //        Console.WriteLine($"Key2: {configurationRoot["key2"]}");
+            //        Console.WriteLine($"key3: {configurationRoot["key3"]}");
+            //    }
+            //);
+            //Console.WriteLine("Begin");
+            #endregion
+
+            #region RegisterChangeCallback
             //token.RegisterChangeCallback(state =>
             //{
             //    Console.WriteLine($"Key1: {configurationRoot["key1"]}");
@@ -35,8 +56,17 @@ namespace ConfiguraionFileDemo2
             //Console.WriteLine($"Key1: {configurationRoot["key1"]}");
             //Console.WriteLine($"Key2: {configurationRoot["key2"]}");
             //Console.WriteLine($"key3: {configurationRoot["key3"]}");
+            #endregion
+
             Console.ReadKey();
 
         }
+    }
+
+    class Config
+    {
+        public string Key1 { get; set; }
+        public bool Key5 { get; set; } = false;
+        public int Key6 { get; private set; } = 100;
     }
 }
