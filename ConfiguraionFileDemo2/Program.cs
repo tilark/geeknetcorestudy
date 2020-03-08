7caa3e4c40342341bdd80f4c0616ca028cf4f9c2
tilark <tilark.liu@gmail.com>
2020-03-08 20:54:33 +0800

    使用程序监听配置热更新
    需要记录配置源的变更时
    需要在配置数据变更时触发特定操作时

--- ConfiguraionFileDemo2/Program.cs
+++ ConfiguraionFileDemo2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
 namespace ConfiguraionFileDemo2
 {
     class Program
@@ -9,21 +10,33 @@ namespace ConfiguraionFileDemo2
             Console.WriteLine("Hello World!");
 
             var builder = new ConfigurationBuilder();
-            builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange:true);
-            builder.AddIniFile("appsetting.ini");
-            builder.AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true);
+            builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
             var configurationRoot = builder.Build();
 
-            Console.WriteLine($"Key1: {configurationRoot["key1"]}");
-            Console.WriteLine($"Key2: {configurationRoot["key2"]}");
-            Console.WriteLine($"key3: {configurationRoot["key3"]}");
-            Console.ReadKey();
+            IChangeToken token = configurationRoot.GetReloadToken();
 
+            ChangeToken.OnChange(() =>
+                configurationRoot.GetReloadToken(),
+                () =>
+                {
+                    Console.WriteLine($"Key1: {configurationRoot["key1"]}");
+                    Console.WriteLine($"Key2: {configurationRoot["key2"]}");
+                    Console.WriteLine($"key3: {configurationRoot["key3"]}");
+                }
+            );
+            Console.WriteLine("Begin");
+            //token.RegisterChangeCallback(state =>
+            //{
+            //    Console.WriteLine($"Key1: {configurationRoot["key1"]}");
+            //    Console.WriteLine($"Key2: {configurationRoot["key2"]}");
+            //    Console.WriteLine($"key3: {configurationRoot["key3"]}");
+            //}, configurationRoot);
             //Console.WriteLine($"Key1: {configurationRoot["key1"]}");
             //Console.WriteLine($"Key2: {configurationRoot["key2"]}");
             //Console.WriteLine($"key3: {configurationRoot["key3"]}");
-            //Console.ReadKey();
+            Console.ReadKey();
+
         }
     }
 }
