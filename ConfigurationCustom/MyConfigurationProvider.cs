97cb8973166f5d363a05996ab5ed357a421aa941
tilark <tilark.liu@gmail.com>
2020-03-08 22:17:51 +0800

    addConfigurationCustom

---
+++ ConfigurationCustom/MyConfigurationProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+using Microsoft.Extensions.Configuration;
+namespace ConfigurationCustom
+{
+    /// <summary>
+    /// 定义一个定时器，每3秒钟刷新一次配置，配置的Key为"lastTime"
+    /// </summary>
+    internal class MyConfigurationProvider : ConfigurationProvider
+    {
+        Timer timer;
+        public MyConfigurationProvider():base()
+        {
+            timer = new Timer();
+            timer.Elapsed += Timer_Elapsed;
+            timer.Interval = 3000;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Load(true);
+        }
+
+        public override void Load()
+        {
+            Load(false);
+        }
+
+        void Load(bool reload)
+        {
+            this.Data["lastTime"] = DateTime.Now.ToString();
+            if (reload)
+            {
+                base.OnReload();
+            }
+        }
+    }
+}
