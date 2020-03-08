c1aeb98c0d29bb694b06c7360f7f95970b7ab1f0
tilark <tilark.liu@gmail.com>
2020-03-08 16:19:44 +0800

    AddConfigurationEnvironmentVariablesDemo

---
+++ ConfigurationEnvironmentVariablesDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+namespace ConfigurationEnvironmentVariablesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var builder = new ConfigurationBuilder();
+            //builder.AddEnvironmentVariables();
+
+            //var configurationRoot = builder.Build();
+            //Console.WriteLine($"key1:{configurationRoot["key1"]}");
+
+            //#region 分层键
+            //var section = configurationRoot.GetSection("SECTION1");
+            //Console.WriteLine($"KEY3:{section["KEY3"]}");
+
+            //var section2 = configurationRoot.GetSection("SECTION1:SECTION2");
+            //Console.WriteLine($"KEY4:{section2["KEY4"]}");
+
+            //#endregion
+
+            #region 前缀过滤
+            builder.AddEnvironmentVariables("XIAO_");
+            var configurationRoot = builder.Build();
+            Console.WriteLine($"key1:{configurationRoot["KEY1"]}");
+            Console.WriteLine($"KEY2:{configurationRoot["KEY2"]}");
+
+            #endregion
+        }
+    }
+}
