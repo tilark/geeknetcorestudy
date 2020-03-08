988e78f3084e1c8bf91d9aa03ec7b40b98e1cbc0
tilark <tilark.liu@gmail.com>
2020-03-08 20:29:37 +0800

    addConfigurationFileDemo
    1. 利用配置文件设置配置参数。
    2. 注意文件可选和热更新等选项。
    3. 后加载的文件会覆盖前面加载的文件。

---
+++ ConfiguraionFileDemo2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+namespace ConfiguraionFileDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange:true);
+            builder.AddIniFile("appsetting.ini");
+            builder.AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true);
+
+            var configurationRoot = builder.Build();
+
+            Console.WriteLine($"Key1: {configurationRoot["key1"]}");
+            Console.WriteLine($"Key2: {configurationRoot["key2"]}");
+            Console.WriteLine($"key3: {configurationRoot["key3"]}");
+            Console.ReadKey();
+
+            //Console.WriteLine($"Key1: {configurationRoot["key1"]}");
+            //Console.WriteLine($"Key2: {configurationRoot["key2"]}");
+            //Console.WriteLine($"key3: {configurationRoot["key3"]}");
+            //Console.ReadKey();
+        }
+    }
+}
