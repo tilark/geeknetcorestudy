33e3cc121e26927e43216041de62d7b2fddd55c8
tilark <tilark.liu@gmail.com>
2020-03-08 22:54:27 +0800

    addOptionDemo
    > 使用``IOptions``使服务与配置框架隔离，服务只需要知道所要值（只依赖OrderServiceOptions），而不关心值的来源。
    > 可为不同的服务配置不同的``Options``

---
+++ OptionsDemo/Services/OrderService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+namespace OptionsDemo.Services
+{
+    public interface IOrderService
+    {
+        int ShowMaxOrderCount();
+    }
+    public class OrderService : IOrderService
+    {
+        IOptions<OrderServiceOptions> _options;
+        public OrderService(IOptions<OrderServiceOptions> options)
+        {
+            this._options = options;
+        }
+        public int ShowMaxOrderCount()
+        {
+            return _options.Value.MaxOrderCount;
+        }
+    }
+
+    public class OrderServiceOptions
+    {
+        public int MaxOrderCount { get; set; } = 100;
+    }
+}
