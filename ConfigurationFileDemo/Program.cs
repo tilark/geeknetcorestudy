ef70e1eaeb01c1cbea89d08e23adb1a0fe622670
tilark <tilark.liu@gmail.com>
2020-03-09 16:43:21 +0800

    AddConfigurationFileDemo

---
+++ ConfigurationFileDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+namespace ConfigurationFileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            var configurationRoot = builder.Build();
+
+            Console.WriteLine($"Key1:{configurationRoot["Key1"]}");
+            Console.WriteLine($"Key2:{configurationRoot["Key2"]}");
+            Console.WriteLine($"Key3:{configurationRoot["Key3"]}");
+
+            Console.ReadKey();
+
+            Console.WriteLine($"Key1:{configurationRoot["Key1"]}");
+            Console.WriteLine($"Key2:{configurationRoot["Key2"]}");
+            Console.WriteLine($"Key3:{configurationRoot["Key3"]}");
+
+            Console.ReadKey();
+        }
+    }
+}
