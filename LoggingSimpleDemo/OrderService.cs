3f2d3ce0679966b7c2cc9196cca6d77bddb14711
tilark <tilark.liu@gmail.com>
2020-03-11 17:26:51 +0800

    日志框架-

---
+++ LoggingSimpleDemo/OrderService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggingSimpleDemo
+{
+    class OrderService
+    {
+        private readonly ILogger<OrderService> _logger;
+        public OrderService(ILogger<OrderService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void Show()
+        {
+            // 输出到Console时再拼接
+            _logger.LogInformation("Show Time {time}", DateTime.Now);
+
+            // 先拼接，再输出。 当不需要输出时，仍然会拼接，有性能损耗。
+            _logger.LogInformation($"Show Time {DateTime.Now}");
+        }
+    }
+}
---
+++ LoggingSimpleDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingSimpleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            var config = builder.Build();
+
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IConfiguration>(p => config);
+
+            serviceCollection.AddLogging(builder =>
+            {
+                builder.AddConfiguration(config.GetSection("Logging"));
+                builder.AddConsole();
+            });
+            serviceCollection.AddTransient<OrderService>();
+            IServiceProvider service = serviceCollection.BuildServiceProvider();
+
+            var order = service.GetService<OrderService>();
+            order.Show();
+            ILoggerFactory loggerFactory = service.GetService<ILoggerFactory>();
+
+            ILogger alogger = loggerFactory.CreateLogger("alogger");
+            alogger.LogDebug(2001, "aiya");
+            alogger.LogInformation("hello");
+
+            var ex = new Exception("出错了");
+            alogger.LogError(ex, "出错了!");
+            Console.ReadKey();
+        }
+    }
+}
