8e5e96da9720c924c2ce8442da84faf5710f4823
tilark <tilark.liu@gmail.com>
2020-03-13 17:58:38 +0800

    AddException
    1. 未完全理解该篇内容。

---
+++ ExceptionDemo/Controllers/ErrorController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Diagnostics;
+using ExceptionDemo.Exceptions;
+using Microsoft.AspNetCore.Authorization;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ExceptionDemo.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorController : Controller
+    {
+        // GET: /<controller>/
+        [Route("/error")]
+        public IActionResult Index()
+        {
+            var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            var ex = exceptionHandlerPathFeature?.Error;
+
+            var knownException = ex as IKnownException;
+            if (knownException == null)
+            {
+                var logger = HttpContext.RequestServices.GetService<ILogger<MyExceptionFilterAttribute>>();
+                logger.LogError(ex, ex.Message);
+                knownException = KnownException.Unknown;
+            } else
+            {
+                knownException = KnownException.FromKnownException(knownException);
+            }
+            return View(knownException);
+        }
+    }
+}
