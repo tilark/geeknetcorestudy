7d0bb932ec658cf99efa9931efa31d97f3691c4b
tilark <tilark.liu@gmail.com>
2020-03-16 17:54:42 +0800

    AddRoutingDemo2

---
+++ RoutingDemo/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RoutingDemo.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    //[Route("api/[controller]/[action]")]
+    [ApiController]
+   
+    public class OrderController : ControllerBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+       [HttpGet("{id:MyRouteConstraint}")]
+       public bool OrderExist(object id)
+        {
+            return true;
+        }
+    }
+}
